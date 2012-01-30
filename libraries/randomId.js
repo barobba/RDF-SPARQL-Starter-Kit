927220d36ec889d28510e2d320b7f69c275deff0
Biagio Arobba <barobba@gmail.com>
2012-01-30 14:49:27 -0700

    Added random number algorithms (for generating UUIDs).
    Signed-off-by: Biagio Arobba <barobba@gmail.com>

--- libraries/randomId.js
+++ libraries/randomId.js
@@ -1,6 +1,26 @@
 
 function generateRandomId(key) {
   var r = Math.random();
-  var id = key + "_" + r.toString();
+  var id = key + r.toString();
   return id;
+}
+
+// From YUI; slightly modified
+function generateRandomId2(key) {
+  
+  // Added key variable
+  if (key) {
+    var id = key;
+  }
+  else {
+    id = '';
+  }
+  
+  // YUI code
+  var  i = 4;
+  while (i--) {
+    id += (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
+  }
+  return id;
+  
 }
