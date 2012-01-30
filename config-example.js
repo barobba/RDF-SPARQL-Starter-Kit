0b4eca2df7bfaf77afb1a90cc718a9e369068b29
Biagio Arobba <barobba@gmail.com>
2012-01-30 14:53:25 -0700

    Added "currentUuidProvider" variable, and re-arranged variables.
    Signed-off-by: Biagio Arobba <barobba@gmail.com>

--- config-example.js
+++ config-example.js
@@ -2,9 +2,13 @@
 resultLimit = 1000;
 
 currentEndpoint = 'http://localhost/rdf-store/endpoint.php';
-currentGraph = 'http://example.com/graphs/Graph1';
 //currentKeyForReading = 'READ_KEY';   // passed as key=READ_KEY (used by some "ARC2" endpoints)
 //currentKeyForWriting = 'WRITE_KEY';  // passed as key=WRITE_KEY (used by some "ARC2" endpoints)
+//currentUuidProvider = 'http://URI/THAT/RETURNS/A/RANDOM/STRING';
+
+currentRdfScope = 'http://example.com/rdf/';
+currentGraph = currentRdfScope + 'graphs/graph1';
+
 
 namespaces = [{
   prefix: 'dc:',
